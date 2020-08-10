6386576d8e9fc0240e102d600f049fc852fc17ea
Mike MacKenzie <mike_mack7@hotmail.com>
2020-08-10 12:16:23 -0400

    All files

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+
+  const query = req.body.cityName;
+  const apiKey = "e1e0b5579e276e75f6e80d58ff27292a";
+  const units = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units;
+
+  https.get(url, function(response){
+      response.on("data", function(data){
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const description = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
+        res.write("<p>The weather is " + description + "</p>");
+        res.write("<img src=" + imageURL + ">");
+        res.send();
+      });
+  });
+});
+
+
+app.listen(3000, function(){
+  console.log("Running 3000");
+});
